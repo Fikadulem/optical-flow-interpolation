4e047e86562829b0205f137682f8ea9815a8b04f
fikadulem <fikadulem@gmail.com>
2025-12-01 12:37:19 -0800

    Added and implemented spatial_regularization.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-#include "include/symmetric_flow.hpp"
+#include "include/computeSymmetricFlow.h"
+#include <opencv2/opencv.hpp>
+
+cv::Mat warpFrame(const cv::Mat &frame, const cv::Mat &flow) {
+    cv::Mat map_x(frame.size(), CV_32FC1);
+    cv::Mat map_y(frame.size(), CV_32FC1);
+
+    for (int y = 0; y < frame.rows; ++y) {
+        for (int x = 0; x < frame.rows; ++x) {
+            cv::Point2f f = flow.at<cv::Point2f>(y, x);
+            map_x.at<float>(y, x) = x + f.x;
+            map_y.at<float>(y, x) = y + f.y;
+        }
+
+    }
+    cv::Mat warped;
+    cv::remap(frame, warped, map_x, map_y, cv::INTER_LINEAR, cv::BORDER_REFLECT);
+    return warped;
+
+}
 
 int main() {
     std::cout << "Program started!" << std::endl;
