7797210f799f2bce05724f506d10dd682675a54a
fikadulem <fikadulem@gmail.com>
2025-12-06 20:34:44 -0800

    Compute Mean Absolute Interpolation Error (MAIE)

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,69 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/ximgproc.hpp>
 #include "computeSymmetricFlow.h"
 #include "warpUtils.h"
 #include <iostream>
 
+// Compute Mean Absolute Interpolation Error
+double computeMAIE(const cv::Mat& pred, const cv::Mat& gt) {
+    CV_Assert(pred.size() == gt.size());
+    CV_Assert(pred.type() == gt.type());
+
+    cv::Mat diff;
+    cv::absdiff(pred, gt, diff);
+    cv::Scalar mae = cv::mean(diff);
+
+    double maie = 0.0;
+    for (int i = 0; i<diff.channels(); ++i) {
+        maie += mae[i];
+    }
+    return maie / diff.channels();
+
+    }
+
+
 // main fnction
-
 int main(int argc, char** argv) {
-    std::string videoPath = "video.mp4";
-    if (argc >= 2) videoPath = argv[1];
 
-    cv::VideoCapture cap(videoPath);
-    if (!cap.isOpened()) {
-        std::cerr << "Cannot open video file: " << videoPath << "\n";
+    if (argc < 4) {
+        std::cerr << "Usage: " <<argv[0] << " <frame10> <frame11> <frame10i1>\n";
         return -1;
     }
 
-    cv::Mat frame0, frame1;
-    cap >> frame0;
-    cap >> frame1;
-
-    if (frame0.empty() || frame1.empty()) {
-        std::cerr << "Not enough frames to interpolate.\n";
+    cv::Mat frame10 = cv::imread(argv[1]);
+    cv::Mat frame11 = cv::imread(argv[2]);
+    cv::Mat frame10i11 = cv::imread(argv[3]);
+    if (frame10.empty() || frame11.empty() || frame10i11.empty()) {
+        std::cerr << "Error reading input images.\n";
         return -1;
     }
 
+    if (frame10.size() != frame11.size()) {
+        std::cerr << "Input frames must have the same size.\n";
+    }
+
+    // Compute symmetric flow
     cv::Mat vs;
-    if (!computeSymmetricFlowTVL1(frame0, frame1, vs)) {
+    if (!computeSymmetricFlowTVL1(frame10, frame11, vs)) {
         std::cerr << "Failed to compute symmetric flow.\n";
         return -1;
     }
 
-    cv::Mat interpolated = interpolateSymmetric(frame0, frame1, vs);
 
-    cv::imshow("Frame 0", frame0);
-    cv::imshow("Frame 1", frame1);
+    cv::Mat interpolated = interpolateSymmetric(frame10, frame11, vs);
+
+    // Compute Mean Absolute Interpolation Error
+    double maie = computeMAIE(interpolated, frame10i11);
+    std::cout << "Mean Absolute Interpolation Error = " << maie << std::endl;
+
+    cv::imshow("Frame 10", frame10);
+    cv::imwrite("frame10.jpg", frame10);
+    cv::imshow("Frame 11", frame11);
+    cv::imwrite("frame11.jpg", frame11);
     cv::imshow("Interpolated Midpoint", interpolated);
+    cv::imwrite("interpolated_midpoint.jpg", interpolated);
     cv::waitKey(0);
 
     return 0;
